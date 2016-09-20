322ef9e21bee443210c4268840c88d76b547ffc8
Pavan Vittala <avittala@gmu.edu>
2016-09-19 22:07:01 -0400

    Fixed indentation, added comments

--- scripts/dataEntry.js
+++ scripts/dataEntry.js
@@ -1,6 +1,10 @@
+//Pavan Vittala
+//Scripts for dataEntry.html
 
+//Function for submitting data to be organized into a table
 $(document).ready(function() {
     $("#submit-data").click(function(){
+        //get panel and input boxes
         var panel = document.getElementById("inputPanel");
         var firstName = document.getElementById("first-name");
         var lastName = document.getElementById("last-name");
@@ -8,25 +12,27 @@ $(document).ready(function() {
         var passwd = document.getElementById("ps");
         var confirmPasswd = document.getElementById("confirm-ps");
             
+        //Create a reference array, used later to create table
         var reference = ["First Name: ", "Last Name: ", "Email: ", "Password: ", "Confirm Password: "];
         var inputData;
-        if (firstName.value != "" && lastName.value != "" && email.value != "" && passwd.value != "" && confirmPasswd.value != "") {
+        if (firstName.value != "" && lastName.value != "" && email.value != "" && passwd.value != "" && confirmPasswd.value != "") {    //All input boxes must have text in them
             inputData = [firstName.value, lastName.value, email.value, passwd.value, confirmPasswd.value];
             inputValidationView(reference, inputData);
-        } else {
+        } else {    //Alert user to fill in all text boxes.
             window.alert("Please fill in all the text boxes");
         }
     });
 });
 
+//Generates table from user input
 function inputValidationView(reference, listOfData) {
     var panel = document.getElementById("inputPanel");
     var body = document.body;
     var table = document.createElement("table");
     table.style.border = "1px solid black";
-    for (var i = 0; i<5; i++) {
+    for (var i = 0; i<5; i++) { //Create 5 rows
         var row = table.insertRow();
-        for (var j = 0; j<2; j++) {
+        for (var j = 0; j<2; j++) { //Create "Placeholder cells"
             var col = row.insertCell();
             if (j == 0) {
                 col.appendChild(document.createTextNode(reference[i]));   
@@ -37,8 +43,10 @@ function inputValidationView(reference, listOfData) {
         }
     }
     var input = document.getElementById("input");
+    //Next two lines are used to store history state
     history.pushState({"state1":1, "data":panel.innerHTML}, "dataView", "?view=dataView");
     history.pushState({"state2":2, "data":panel.innerHTML}, "dataView", "?view=dataView");
+    //Remove the data in the old panel
     panel.removeChild(input);
     table.marginTop = "100px";
     table.marginBottom = "100px";
@@ -48,12 +56,14 @@ function inputValidationView(reference, listOfData) {
     table.style.fontWeight = "none";
     var title = document.createElement("h2");
     title.appendChild(document.createTextNode("Your Information: "));
+    //Add new information to the panel
     panel.appendChild(title);
     panel.appendChild(table);
+    //Let the user know this feature is yet to come!
     window.alert("This feature is not yet complete... Check back soon! But here's your information for you!");
 }
 
-//dataInputValidationPanelView
+//dataInputValidationPanelView, used when user hits back or forward button
 window.addEventListener('popstate', function(event) {
     event.preventDefault();
     var state = event.state;
