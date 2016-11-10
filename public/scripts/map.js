8612027547f05f5d7baaf2d894b5afaa058ee147
Pavan Vittala <avittala@gmu.edu>
2016-11-09 22:09:40 -0500

    Implemented Heuristics
    2. Implemented autocomplete based search; autocomplete based directions
    still needs to be implemented
    3. Implemented spinner for directions search and regular search

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -14,12 +14,143 @@ function clearMarkers() {
     markersArray.length = 0;
 }
 
+/*
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.850033, lng: -87.6500523},
         zoom: 4
     });
 }
+*/
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.850033, lng: -87.6500523},
+        zoom: 4
+    });
+    var searchInput = /** @type {!HTMLInputElement} */(
+        document.getElementById('pac-input'));
+        var types = document.getElementById('type-selector');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchInput);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+        var searchAutocomplete = new google.maps.places.Autocomplete(searchInput);
+        searchAutocomplete.bindTo('bounds', map);
+
+        /*
+        var fromAutocomplete = new google.maps.places.Autocomplete(input);
+        searchAutocomplete.bindTo('bounds', map);
+        var toAutocomplete = new google.maps.places.Autocomplete(input);
+        searchAutocomplete.bindTo('bounds', map);
+        */
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        searchAutocomplete.addListener('place_changed', function() {
+            $('#spinner').toggle();
+            clearMarkers();
+            if (directionsDisplay != null) {
+                directionsDisplay.set('directions', null);
+            }
+            infowindow.close();
+          marker.setVisible(false);
+          var place = searchAutocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            //window.alert("No details available for input: '" + place.name + "'");
+            clearMarkers();
+            if (directionsDisplay != null) {
+                directionsDisplay.set('directions', null);
+            }
+            //var searchInput = $("#searchInput").val()   //The input typed by the user into the search box
+            var searchInput = $("#pac-input").val();
+            //console.log(searchInput);
+            var service = new google.maps.places.PlacesService(map);    //Google service that handles Places
+            var infoWindow = new google.maps.InfoWindow({map: map});
+            var lat;
+            var lng;
+            var currentLocation;
+            if (navigator.geolocation) {    //If the navigator supports geolocation
+                navigator.geolocation.getCurrentPosition(function(position) {   //Retrieve data about the current position
+                    var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+
+                    };
+                    lat = position.coords.latitude;
+                    lng = position.coords.longitude;
+                    currentLocation = new google.maps.LatLng(lat, lng);
+                    var request = { //Create a request for PlacesService
+                        location: currentLocation,
+                        radius: '100',
+                        query: searchInput
+                    };
+                    service.textSearch(request, callback);
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('You are here');  //Put infopane on current location
+                    map.setCenter(pos); //Center on the current position
+                    map.setZoom(15);    //Zoom appropriately
+                    $('#spinner').toggle();
+                }, function() { //Handle location error
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+            } else {
+                // Browser doesn't support Geolocation
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
+          $('#spinner').toggle();
+        });
+
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            searchAutocomplete.setTypes(types);
+          });
+        }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+      }
+
 
 //Function that changes the number of tolls displayed in the toll table.
 //function refreshTable() {
@@ -200,14 +331,17 @@ $(document).ready(function() {
     var directionsServiceOriginal = new google.maps.DirectionsService();
     directionsDisplayOriginal.setMap(map);
     $("#submitDirections").click(function () {
+        $('#spinner').toggle();
         clearMarkers();
         directionsDisplay = new google.maps.DirectionsRenderer();
         var directionsService = new google.maps.DirectionsService();
         directionsDisplay.setMap(map);
         directionsDisplay=directionsDisplayOriginal;
         directionsService=directionsServiceOriginal;
-        var start = document.getElementById('fromAddress'); //Origin address
-        var end = document.getElementById('toAddress'); //Destination address
+        //var start = document.getElementById('fromAddress'); //Origin address
+        //var end = document.getElementById('toAddress'); //Destination address
+        var start = document.getElementById('pac-input-dir-from'); //Origin address
+        var end = document.getElementById('pac-input-dir-to'); //Destination address
         if (start.value === "" || end.value === "") {   //Both start and end needs to have values in them
             alert("Either From Address or To Address is empty");
             return;
@@ -222,6 +356,7 @@ $(document).ready(function() {
             if (status == 'OK') {
                 directionsDisplay.setDirections(result);
                 count++;
+                $('#spinner').toggle();
             } else if (status == 'NOT_FOUND') {
                 alert("Origin or destination not found");
                 return;
