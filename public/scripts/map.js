65bc06cabbc7f7467d36c3b5e06c9ac24668318a
Pavan Vittala <avittala@gmu.edu>
2016-11-30 17:30:05 -0500

    Removed Legacy Code from map.js
    Removed Legacy Code from map.js

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -3,10 +3,11 @@
 */
 
 var count = 0;
-var map;
-var markersArray = [];
-var directionsDisplay;
+var map;    //Global reference to the map
+var markersArray = [];  //Holds all of the markers that are placed on the map
+var directionsDisplay; 
 
+/*Function for clearing all the markers in the markersArray*/
 function clearMarkers() {
     while (markersArray.length) {
         markersArray.pop().setMap(null);
@@ -14,15 +15,7 @@ function clearMarkers() {
     markersArray.length = 0;
 }
 
-/*
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 41.850033, lng: -87.6500523},
-        zoom: 4
-    });
-}
-*/
-
+/*Initialize map and run code for setting up autocomplete search system*/
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.850033, lng: -87.6500523},
@@ -36,13 +29,6 @@ function initMap() {
 
         var searchAutocomplete = new google.maps.places.Autocomplete(searchInput);
         searchAutocomplete.bindTo('bounds', map);
-
-        /*
-        var fromAutocomplete = new google.maps.places.Autocomplete(input);
-        searchAutocomplete.bindTo('bounds', map);
-        var toAutocomplete = new google.maps.places.Autocomplete(input);
-        searchAutocomplete.bindTo('bounds', map);
-        */
 
         var infowindow = new google.maps.InfoWindow();
         var marker = new google.maps.Marker({
@@ -67,9 +53,7 @@ function initMap() {
             if (directionsDisplay != null) {
                 directionsDisplay.set('directions', null);
             }
-            //var searchInput = $("#searchInput").val()   //The input typed by the user into the search box
             var searchInput = $("#pac-input").val();
-            //console.log(searchInput);
             var service = new google.maps.places.PlacesService(map);    //Google service that handles Places
             var infoWindow = new google.maps.InfoWindow({map: map});
             var lat;
@@ -111,15 +95,7 @@ function initMap() {
             map.setCenter(place.geometry.location);
             map.setZoom(17);  // Why 17? Because it looks good.
           }
-          /*
-          marker.setIcon(({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-          }));
-            */
+
           marker.setPosition(place.geometry.location);
           marker.setVisible(true);
           var address = '';
@@ -149,124 +125,8 @@ function initMap() {
         setupClickListener('changetype-address', ['address']);
         setupClickListener('changetype-establishment', ['establishment']);
         setupClickListener('changetype-geocode', ['geocode']);
-      }
+}
 
-
-//Function that changes the number of tolls displayed in the toll table.
-//function refreshTable() {
-/*
-$(document).ready(function(){
-    $('#refreshButton').click(function(){
-        var defaultTollNumber = $('#tableOfTolls').data('default');
-        var numTolls = document.getElementById("numTolls").value;   //New number of rows you want
-        var table = document.getElementById("tolltable");   //Save the table object on the page
-        var tableNumRows = table.rows.length-1;   //Number of rows actually there, -1 because we have a label row
-        if (numTolls > defaultTollNumber) {
-            alert("A maximum of 4 tolls can be displayed at once");
-        } else if (numTolls < tableNumRows) {  //Decrease the number of rows
-            var tableCreation = tableNumRows-numTolls;
-            for (var i = 0; i<tableCreation; i++) {
-                table.deleteRow(-1);    //Delete the row, the -1 paramater deletes the last row of the table
-                table.rows.length--;    //Decrement the row value for the table
-                tableNumRows = table.rows.length;
-            }
-        } else if (numTolls > tableNumRows) {   //Increase the number of rows
-            var tableCreation = numTolls - tableNumRows;
-            var row;
-            for (var i = 0; i<tableCreation; i++) { //Add row
-                row = table.insertRow(-1);
-                table.rows.length++;
-                tableNumRows = table.rows.length;
-                for (var j = 0; j<2; j++) { //Add cells to the row
-                    var cell = row.insertCell(j);
-                    cell.innerHTML = "Placeholder";
-                }
-                //Next three lines are for adding a centered checkbox to the third cell in a new row
-                var cell = row.insertCell(-1);
-                cell.innerHTML = "<input type=\"checkbox\" checked=\"checked\"/>"
-                cell.style.textAlign = "center";
-            }
-        }
-    });
-});
-*/
-
-/*
-//Shows/hides address input tab for directions
-$(document).ready(function(){
-    $("#show-hide-button-address").click(function(){
-        var button = document.getElementById("hideAddress");
-        if (button.value == "Hide Directions") {
-            $("#addressInput").hide();
-            button.value = "Show Directions";
-            history.pushState(null, null, "map.html");
-        } else if (button.value == "Show Directions") {
-            $("#addressInput").show();
-            button.value = "Hide Directions";
-            history.pushState(null, null, "map.html");
-        }
-    });
-});
-*/
-
-//Shows/hides toll data tab
-/*
-$(document).ready(function(){
-    $("#show-hide-button-tolls").click(function(){
-        var button = document.getElementById("hideTolls");
-        if (button.value == "Hide Tolls") {
-            $("#tableOfTolls").hide();
-            button.value = "Show Tolls";
-        } else if (button.value == "Show Tolls") {
-            $("#tableOfTolls").show();
-            button.value = "Hide Tolls";
-        }
-    });
-});
-*/
-
-//Function for searching Google Maps for places/ locations
-$(document).ready(function(){
-    $("#submitSearch").click(function() {
-        clearMarkers();
-        if (directionsDisplay != null) {
-            directionsDisplay.set('directions', null);
-        }
-        var searchInput = $("#searchInput").val()   //The input typed by the user into the search box
-        var service = new google.maps.places.PlacesService(map);    //Google service that handles Places
-        var infoWindow = new google.maps.InfoWindow({map: map});
-        var lat;
-        var lng;
-        var currentLocation;
-        if (navigator.geolocation) {    //If the navigator supports geolocation
-            navigator.geolocation.getCurrentPosition(function(position) {   //Retrieve data about the current position
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-
-                };
-                lat = position.coords.latitude;
-                lng = position.coords.longitude;
-                currentLocation = new google.maps.LatLng(lat, lng);
-                var request = { //Create a request for PlacesService
-                    location: currentLocation,
-                    radius: '100',
-                    query: searchInput
-                };
-                service.textSearch(request, callback);
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('You are here');  //Put infopane on current location
-                map.setCenter(pos); //Center on the current position
-                map.setZoom(15);    //Zoom appropriately
-            }, function() { //Handle location error
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-    });
-});
 
 //Callback for a text search in the Google Maps API
 function callback(results, status) {
